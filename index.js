e64b0aba59806eeac8c9f74fe28b5d8e5a8e8b75
Nidhi Nayak <nayaknidhig@gmail.com>
2020-12-26 13:03:57 +0530

    fixed sql for registration

--- index.js
+++ index.js
@@ -47,22 +47,51 @@ app.get("/newHome", (req, res) => {
     if(loginMail === undefined){
         //user signed up
         //check if regMail is in the DB already
+        let sql = "SELECT * FROM user WHERE email ='" + signupMail + "'";
         //if it isnt, then enter regMail,regPass,regName to DB
-        //if it exists, ask user to log in and go to /homenew
-        res.render('homenew',{});
+        con.query(sql, (err, result) => {
+            if (err) throw err;
+
+            if (result.length === 0) {
+                let insertSQL = "INSERT INTO `user` (`fullName`, `email`, `password`) VALUES ('" +
+                signupName +"', '"+ signupMail + "', '" + signupPass +"')";
+            //if it exists, ask user to log in and go to /homenew
+            con.query(insertSQL, (err) => {
+                if (err) throw err;
+                console.log("Successful Added User");
+                res.render('homenew',{});
+        });
+        } else {
+        res.sendFile(__dirname + "/views/Error.html");
+        }
+    });
     } else if(signupMail === undefined) {
         // user logged in
+        console.log("User Logged In ");
+        app.set('loginMail', loginMail);
+        let sqlu = "SELECT * FROM user WHERE email ='" + loginMail + "'";
+        let sqlp = "SELECT * FROM user WHERE password ='" + loginPass + "'";
+        con.query(sqlu, (err, result) => {
+            if (err) throw err;
         //check if logMail exists in DB
+            if (result.length === 1) {
+                console.log("User Exists in DB");
+                con.query(sqlp, (err, result) => {
+                    if (err) throw err;
+                    if (result.length >= 1) {
+                        res.render('homenew',{});
+                    } else {
+                        console.log("Wrong Password has been entered");
+                        res.sendFile(__dirname + "/views/Error.html");
+                    }
+                });
         //if it doesnt, then ask user to Register
         //if it exists, go to /homenew
-        res.render('homenew',{});
-    }
-//let sql = "INSERT INTO `user`() VALUES('" + name + "','" + email + "','" + password + "');";
-    //con.query(sql, (err,result) => {
-    //     if (err) throw err;
-    //     console.log(result[0].fullname)
-    // });
-});
+                res.render('homenew',{});
+                }
+            });
+        }
+    });
 
 //------------------------------------------------------------------//
 
